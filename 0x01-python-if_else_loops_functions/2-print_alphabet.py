49af293cb3a1043fa0196f5607e82d9002093b1b
Eli Greene <1697@holbertonschool.com>
2020-12-09 04:16:24 +0000

    added 4 and 5, fixed 2 and 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for x in range(26):
-    print("{}".format(chr(x + 97)))
+    print("{}".format(chr(x + 97)), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(26):
     if chr(x + 97) is not 'q' and chr(x + 97) is not 'e':
-        print("{}".format(chr(x + 97)))
+        print("{:s}".format(chr(x + 97)), end="")
