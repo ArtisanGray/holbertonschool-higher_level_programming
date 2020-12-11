c5607d11eccdaeff8ffd52414fd22c313d94587a
ArtisanGray <1697@holbertonschool.com>
2020-12-11 02:14:33 +0000

    fixed 2 so it displays correct nomenclature

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 import sys
 if __name__ == "__main__":
     num = 0
-    print("{:d} arguments".format(len(sys.argv) - 1), end="")
+    print("{:d} argument".format(len(sys.argv) - 1), end="")
     if len(sys.argv) == 1:
         print(".")
-    else:
+    elif len(sys.argv) == 2:
         print(":")
+    else:
+        print("s:")
     for arg in sys.argv:
         num += 1
         if num > 1 and len(sys.argv) > 1:
