e6166159f0c842e46948219583cad56cc07e0f8f
ArtisanGray <1697@holbertonschool.com>
2020-12-18 02:42:50 +0000

    fixed 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
+def sq(x):
+	return(x * x)
+
 def square_matrix_simple(matrix=[]):
     if matrix is None:
         return
     nm = []
     for i in range(len(matrix)):
-        nm.append(list(map(lambda x: x * x, matrix[i])))
+        nm.append(list(map(sq, matrix[i])))
     return(nm)
