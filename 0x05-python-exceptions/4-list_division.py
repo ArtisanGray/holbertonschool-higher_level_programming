73ff4671ecadba260c10e6312e1b60a46ae04a7d
ArtisanGray <1697@holbertonschool.com>
2021-01-04 23:37:05 +0000

    forgot to add finally test case

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,20 +7,21 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             if (i < list_length):
                 div = my_list_1[i]/my_list_2[i]
-                divlist.append(div)
+                i += 1
             else:
                 break
-            i += 1
         except TypeError:
-            divlist.append(0)
+            div = 0
             print("wrong type")
             i += 1
         except ZeroDivisionError:
-            divlist.append(0)
+            div = 0
             print("division by zero")
             i += 1
         except IndexError:
-            divlist.append(0)
+            div = 0
             print("out of range")
             break
+        finally:
+            divlist.append(div)
     return(divlist)
