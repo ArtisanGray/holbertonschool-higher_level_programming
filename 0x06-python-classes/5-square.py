40ce8c3206c3ceec1e980723c8b3e71ee6ea82c4
ArtisanGray <1697@holbertonschool.com>
2021-01-06 00:28:15 +0000

    testing doc choice on 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
+"""placeholder module doc thing, no idea what goes here
+"""
+
+
 class Square:
-    '''square class'''
+    """square class with different methods
+    """
     def __init__(self, size=0):
-        '''initializes the size of the square and any other data needed'''
+        """initializes the size of the square and any other data needed
+
+        Args:
+            size(int): size of the square
+        """
         self.__size = size
 
     @property
     def size(self):
-        '''returns the current size of the square'''
+        """returns the current size of the square
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        '''Sets the size of the square, checks for errors/bad type'''
+        """Sets the size of the square, checks for errors/bad types
+
+        Args:
+            value(int): value to set the size to
+        """
         if isinstance(value, int) is not True:
             raise TypeError('size must be an integer')
         if value < 0:
@@ -20,11 +34,13 @@ class Square:
         self.__size = value
 
     def area(self):
-        '''returns the area of the square'''
+        """returns the area of the square
+        """
         return (self.__size ** 2)
 
     def my_print(self):
-        '''prints the square'''
+        """prints the square
+        """
         if (self.__size) != 0:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
