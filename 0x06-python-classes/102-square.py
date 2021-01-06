b0f3a645973b66e2d799c1822aec1e8868cbfdfd
ArtisanGray <1697@holbertonschool.com>
2021-01-06 02:09:51 +0000

    that was kind of fun to research, added 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""this module contains square class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """initializes the size of the square and any other data needed
+        Args:
+            size(int): the size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """returns the current size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square, checks for errors/bad type
+        Args:
+            value(int): value to resize square with
+        """
+        if isinstance(value, int) is not True:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """returns the area of the square"""
+        return (self.__size ** 2)
+
+    def __new__(self, area):
+        """ returns area of square using an alternative to init """
+        return area
