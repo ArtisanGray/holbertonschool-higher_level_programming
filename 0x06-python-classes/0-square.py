e81383934855ba75cc29683cf9ed2fd437cadde8
ArtisanGray <1697@holbertonschool.com>
2021-01-06 00:29:54 +0000

    testing doc choice on 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""this module contains a square class that creates a square, size, area"""
+
+
 class Square():
-    '''square class, does nothing'''
+    """square class, does nothing"""
     pass
