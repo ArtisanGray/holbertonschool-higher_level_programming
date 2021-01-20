bde6037fd70a72f3e48538cc701455c5b5bbdc10
ArtisanGray <1697@holbertonschool.com>
2021-01-20 05:03:33 +0000

    typo fix for 0, and correct module descriptor

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""this module checks if two instances are the same class and type"""
+"""not a module but returns a list of available methods and attributes"""
 
 
 def lookup(obj):
-    """returns the list of available data from class"""|
+    """returns the list of available data from class"""
     return(dir(obj))
