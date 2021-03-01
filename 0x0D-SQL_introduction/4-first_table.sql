dde1e831cdaf31b3b86275b6b2f549bacae10911
ArtisanGray <1697@holbertonschool.com>
2021-03-01 23:57:03 +0000

    tsting 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- creates a new table in the given database.
 CREATE TABLE IF NOT EXISTS first_table(
-	id INT			DEFAULT NULL,
-	name VARCHAR(256)	DEFAULT NULL,
-	PRIMARY KEY (ID)
+	id INT DEFAULT NULL,
+	name VARCHAR(256) DEFAULT NULL,
 );
