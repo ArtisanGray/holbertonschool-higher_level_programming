64326be5e819b6d17bba4b5da3de6afcac3d8401
ArtisanGray <1697@holbertonschool.com>
2021-03-02 21:28:39 +0000

    i mustve not read something, these second comment lines are just weird...

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- creates a user with certain permissions
+-- seriously why did the two comment thing work on 0, does it work here?
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,7 @@
 -- creates a table with unique values enforced.
+-- noticed that mySQL uses different nomenclature/structure.
 CREATE TABLE IF NOT EXISTS 'unique_id' (
-	id INT DEFAULT 1,
+	id INT NOT NULL DEFAULT 1,
 	name VARCHAR (256),
 	UNIQUE (id)
 );
