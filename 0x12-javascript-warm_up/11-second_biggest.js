e6474895b84962c34b95084cd863536e3921e254
ArtisanGray <1697@holbertonschool.com>
2021-08-02 22:40:56 +0000

    fixed 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length > 3) {
-  let newarg = process.argv.slice(2);
+  const newarg = process.argv.slice(2);
   newarg.sort(function (a, b) { return b - a; });
   console.log(newarg[1]);
 } else {
