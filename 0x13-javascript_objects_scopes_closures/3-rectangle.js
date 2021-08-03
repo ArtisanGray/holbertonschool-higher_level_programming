3828b4802b26cd68dbc3a2582057b7e8ad81ff16
ArtisanGray <1697@holbertonschool.com>
2021-08-03 19:09:14 +0000

    fixed output on 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,13 +7,13 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-  print () {
 
+  print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write('X');
       }
       process.stdout.write('\n');
     }
-  };
-}
+  }
+};
