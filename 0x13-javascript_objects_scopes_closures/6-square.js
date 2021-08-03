bedd9775a335f0d0ccd5c814b56ff83e4861c049
ArtisanGray <1697@holbertonschool.com>
2021-08-03 19:52:50 +0000

    may have fixed 6, inheritance requirement

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,9 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-      }
-      process.stdout.write('\n');
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
+const oldSqr = require('./5-square');
+module.exports = class Square extends oldSqr {
   constructor (size) {
-    super(size, size);
+    super(size);
   }
 
   charPrint (c = 'X') {
