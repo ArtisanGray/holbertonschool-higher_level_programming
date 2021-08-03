57763563e610aa12f783cd7381a73bbdba5b2644
ArtisanGray <1697@holbertonschool.com>
2021-08-03 22:13:55 +0000

    an idiosyncracy with indexof occurred, came up with an ingenious solution :)

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-
+let length = -1;
 const data = require('./100-data');
 const flip = data.list.map(x => multi(data.list, x));
 
 function multi (list, item) {
-  const temp = list.indexOf(item);
-  return item * temp;
+  length++;
+  console.log(item + "'s current index is: " + length);
+  return item * length;
 }
 console.log(data.list);
 console.log(flip);
