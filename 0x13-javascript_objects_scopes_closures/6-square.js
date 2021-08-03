8cbb0bca0663bac90c4d318aa9e7fdbb46ef8bdc
ArtisanGray <1697@holbertonschool.com>
2021-08-03 19:54:10 +0000

    useless constructor fix for jessy

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 
 const oldSqr = require('./5-square');
 module.exports = class Square extends oldSqr {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
