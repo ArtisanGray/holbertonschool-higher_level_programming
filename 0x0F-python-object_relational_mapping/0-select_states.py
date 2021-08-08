fd300d7d19c8e5bd1d5bf50d093bcbc6c685e1bd
ArtisanGray <1697@holbertonschool.com>
2021-08-08 09:46:13 +0000

    created essential files and 0, inital test commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     link = MySQLdb.connect(host="localhost", port=3306, user="root",
-                           passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+                           passwd="root", db="hbtn_0e_0_usa")
     ms = link.cursor()
     ms.execute("SELECT * FROM states ORDER BY id ASC")
     data = ms.fetchall()
