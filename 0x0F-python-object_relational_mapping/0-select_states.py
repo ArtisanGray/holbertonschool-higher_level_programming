884b355eef44e839958c325a494cd439b25d31df
ArtisanGray <1697@holbertonschool.com>
2021-08-08 10:12:20 +0000

    fixed output...thing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """this module is for manipulating MySQL databases."""
 import MySQLdb
+import sys
 
-def main(**kwargs):
-    link = MySQLdb.connect(host="localhost", port=3306, **kwargs)
-    ms = link.cursor()
-    ms.execute("SELECT * FROM states ORDER BY id ASC")
-    data = ms.fetchall()
-    i = 0
-    for item in data:
-        print(str(item))
-        i += 1
-    ms.close()
-    link.close()
+if __name__ == "__main__":
+        link = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3])
+        ms = link.cursor()
+        ms.execute("SELECT * FROM states ORDER BY id ASC")
+        data = ms.fetchall()
+        i = 0
+        for item in data:
+            print(str(item))
+            i += 1
+        ms.close()
+        link.close()
