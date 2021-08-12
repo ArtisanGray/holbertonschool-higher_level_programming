5d79e7161c3ed651b308f88c56cf7b811a6f96aa
ArtisanGray <1697@holbertonschool.com>
2021-08-12 16:09:36 +0000

    created 14, initial test commit

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this module uses SQLAlchemy to manipulate sql databases."""
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from model_state import Base, State
+
+class City(Base):
+    """class defining city tables"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
