2bcc61a310be469d41bbe4125dadf64a59e2cce1
ArtisanGray <kitwithshorts@gmail.com>
2021-08-19 22:03:54 +0000

    created 0, initial test commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #requests a url and displays the size of the body of the response.
-if [ "$#" -ne 2 ]; then
-  curl -sI "$1" | grep -i Content-Length
-fi
+curl -sI "$1" | grep -i Content-Length
