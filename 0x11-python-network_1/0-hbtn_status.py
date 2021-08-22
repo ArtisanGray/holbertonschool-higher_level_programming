224d850bef6eb9fefbb6da162d0c1bd64975f92c
ArtisanGray <1697@holbertonschool.com>
2021-08-22 20:19:45 +0000

    created the needed output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,4 +5,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         data = response.read()
-        print(data)
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8', 'strict')))
