a70b7ae2f7e016fb64b8a73fa2142f0bf65a954a
ArtisanGray <1697@holbertonschool.com>
2021-08-22 23:58:24 +0000

    blind change, not tested

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,15 +9,16 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     letter = ""
     if (sys.argv[1]):
-        if (len(sys.argv[1]) > 0):
-            letter = sys.argv[1]
-    data = {'q': letter}
+        letter = sys.argv[1]
+        data = {'q': letter}
+    else:
+        data = {'q': ''}
     r = requests.post(url, data)
     try:
         values = r.json()
-        if len(values) > 0:
+        if values == {} or values is None:
             print("[{}] {}".format(values['id'], values['name']))
         else:
             print("No result")
-    except (requests.exceptions.JSONDecodeError):
+    except Exception:
         print("Not a valid JSON")
