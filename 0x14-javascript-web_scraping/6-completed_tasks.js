2d85bcc35cb473b34c6586e69fa471cb990ba320
ArtisanGray <1697@holbertonschool.com>
2021-09-14 18:06:26 +0000

    fixed semistandard errors and removed var assignment

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const url = process.argv[2];
-var tasks_Completed = {};
+const tasksCompleted = {};
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
@@ -10,12 +10,12 @@ request(url, function (error, response, body) {
   const data = JSON.parse(body);
   for (const item in data) {
     if (data[item].completed === true) {
-      if (tasks_Completed[data[item].userId]) {
-        tasks_Completed[data[item].userId] += 1;
+      if (tasksCompleted[data[item].userId]) {
+        tasksCompleted[data[item].userId] += 1;
       } else {
-        tasks_Completed[data[item].userId] = 1;
+        tasksCompleted[data[item].userId] = 1;
       }
     }
   }
-  console.log(tasks_Completed);
+  console.log(tasksCompleted);
 });
